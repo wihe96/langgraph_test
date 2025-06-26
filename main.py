17b016f50333957ae76fa06ac53ea265ec7285af
wihe96 <wiktorheppler24@gmail.com>
2025-06-26 16:54:20 +0200

    fix: working graph definition

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from langgraph.graph import entrypoint
-from langgraph.graph import StateGraph, END
+
+from langgraph.graph import entrypoint, StateGraph, END
 from langchain_core.runnables import RunnableLambda
 
 @entrypoint
 def build_graph():
     graph = StateGraph(dict)
 
-    def hello(state):
-        return {"msg": "Hello from LangGraph!"}
+    def hello_node(state):
+        return {"text": "Hello from LangGraph!"}
 
-    graph.add_node("start", RunnableLambda(hello))
+    graph.add_node("start", RunnableLambda(hello_node))
     graph.set_entry_point("start")
     graph.set_finish_point(END)
 
     return graph.compile()
+
